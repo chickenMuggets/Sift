ed2c9097b41e7cfae27af77bd599049f6e356a6a
unknown <alexssmith124@protonmail.com>
2025-03-06 17:46:36 +1000

    made a simple program that prints out the columns and rows in a terminal

---
+++ includes/getterminalsize.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
+#endif
+
+int getTerminalSize(int RC = 1) {
+    #ifdef _WIN32
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    int columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+    int rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+    return (RC == 1) ? columns : rows;
+    
+    #else
+    struct winsize w;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == -1) {
+        return -1; // Return -1 if ioctl fails
+    }
+    return (RC == 1) ? w.ws_col : w.ws_row;
+    #endif
+}
--- sift.cpp
+++ sift.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "includes/getterminalsize.h"
 
 int main() {
+    while (true)
+    {
+        std::cout << "rows: " << getTerminalSize(0) << " Columns: " << getTerminalSize(1) << "\n";
+    }
     
     return 0;
 }
