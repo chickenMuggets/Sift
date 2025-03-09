4311996395288ddaa242ff51e068da9bffe63dd7
unknown <alexssmith124@protonmail.com>
2025-03-09 13:42:40 +1000

    cleaned up code

--- includes/getselectedcharacter.cpp
+++ includes/getselectedcharacter.cpp
@@ -4,12 +4,18 @@
 #include <iostream>
 #include <termios.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <sys/select.h>
+#include <sys/ioctl.h>
 #endif
 
 
 #ifdef _WIN32
 int getchrim() {
     return _getch();
+}
+int kbhit() {
+    return _kbhit();
 }
 #else
 int getchrim() {
@@ -26,5 +32,23 @@ int getchrim() {
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);  // Restore the old settings
     
     return ch;
+}
+int _kbhit() {
+    static const int STDIN = 0;
+    static bool initialized = false;
+
+    if (!initialized) {
+        // Use termios to turn off line buffering
+        termios term;
+        tcgetattr(STDIN, &term);
+        term.c_lflag &= ~ICANON;
+        tcsetattr(STDIN, TCSANOW, &term);
+        setbuf(stdin, NULL);
+        initialized = true;
+    }
+
+    int bytesWaiting;
+    ioctl(STDIN, FIONREAD, &bytesWaiting);  // Check if data is available on stdin
+    return bytesWaiting;
 }
 #endif
--- sift.cpp
+++ sift.cpp
@@ -8,39 +8,7 @@
 #include "includes/getselectedcharacter.h"
 #include "includes/consVectorHandler.h"
 #include "includes/keypresshandler.h"
-#ifdef _WIN32
-#include <conio.h>
-#else
-#include <stdio.h>
-#include <sys/select.h>
-#include <sys/ioctl.h>   // Include for ioctl and FIONREAD
-#include <unistd.h>      // Include for STDIN
-#include <termios.h>
-#endif
 
-// Define _kbhit() outside the main function to avoid the function-definition error.
-#ifdef _WIN32
-// Windows-specific _kbhit() implementation can go here if needed.
-#else
-int _kbhit() {
-    static const int STDIN = 0;
-    static bool initialized = false;
-
-    if (!initialized) {
-        // Use termios to turn off line buffering
-        termios term;
-        tcgetattr(STDIN, &term);
-        term.c_lflag &= ~ICANON;
-        tcsetattr(STDIN, TCSANOW, &term);
-        setbuf(stdin, NULL);
-        initialized = true;
-    }
-
-    int bytesWaiting;
-    ioctl(STDIN, FIONREAD, &bytesWaiting);  // Check if data is available on stdin
-    return bytesWaiting;
-}
-#endif    
 
 int main(int argc, char** argv) {
     std::string filedirectory;
